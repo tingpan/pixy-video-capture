5cd2328058aa4abc334717e79009ebeb5e61d450
TingMiao <tingpan666@gmail.com>
2017-03-02 00:23:44 +0000

    ADD: data preprocessing script

---
+++ scripts/crop_video.py
@@ -0,0 +1,135 @@
+import numpy as np
+import cv2
+import sys
+import os
+
+height = 0
+width = 0
+crop_index = [(0, 0), (0, 0)]
+dragMode = 0
+mousePosition = (0, 0)
+
+
+def read_video(video_file, save_dir):
+    cap = cv2.VideoCapture(video_file)
+    i = 0
+
+    while (cap.isOpened()):
+
+        ret, frame = cap.read()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        if i % 5 != 0:
+            i += 1
+            continue
+
+        if frame is None:
+            break
+
+        if i == 0:
+            initiate(frame)
+
+        crop_frame = frame[crop_index[0][1]: crop_index[1][1], crop_index[0][0]: crop_index[1][0]]
+
+        # crop_frame = cv2.resize(crop_frame, (151, 95))
+
+        cv2.imshow('frame', crop_frame)
+
+        cv2.imwrite('%s/%s.jpg' % (save_dir, i / 5 + 218), crop_frame)
+        i += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def initiate(frame):
+    global height, width, crop_index
+
+    [height, width] = frame.shape[:2]
+
+    center_left = (width - height) / 2
+    center_right = (width - height) / 2 + height
+
+    crop_index = [(0, 0), (width, height)]
+
+    showCropImage(frame)
+
+    while 1:
+        if cv2.waitKey(1) & 0xFF == ord('n'):
+            break
+
+
+def showCropImage(frame):
+    alpha = 0.5
+    overlay = frame.copy()
+    output = frame.copy()
+
+    cv2.rectangle(overlay, crop_index[0], crop_index[1], (200, 200, 200), -1)
+    cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+    cv2.rectangle(output, crop_index[0], crop_index[1], (50, 50, 50), 1)
+
+    cv2.imshow('frame', output)
+    cv2.setMouseCallback('frame', mouseHandler, frame)
+
+
+def mouseHandler(event, x, y, flags, frame):
+    global dragMode, mousePosition, crop_index
+    crop_thread = 8
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        mousePosition = (x, y)
+
+        diff = [x - crop_index[0][0], x - crop_index[1][0], y - crop_index[0][1], y - crop_index[1][1]]
+
+        if (abs(diff[0]) < crop_thread or abs(diff[1]) < crop_thread) and (
+                abs(diff[2]) < crop_thread or abs(diff[3]) < crop_thread):
+            dragMode = 1
+
+        elif diff[0] > 0 and diff[1] < 0 and diff[2] > 0 and diff[3] < 0:
+            dragMode = 2
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+
+        if dragMode == 1:
+            print 'crop'
+
+        elif dragMode == 2:
+
+            d_x = x - mousePosition[0]
+            d_y = y - mousePosition[1]
+
+            crop_width = crop_index[1][0] - crop_index[0][0]
+            crop_height = crop_index[1][1] - crop_index[0][1]
+
+            new_x = crop_index[0][0] + d_x
+            new_y = crop_index[1][1] + d_y
+
+            if new_x < 0: new_x = 0
+            if new_x + crop_width > width: new_x = width - crop_width
+
+            if new_y > height: new_y = height
+            if new_y - crop_height < 0: new_y = crop_height
+
+            crop_index = [(new_x, new_y - crop_height), (new_x + crop_width, new_y)]
+            mousePosition = (x, y)
+
+        if dragMode != 0:
+            showCropImage(frame)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        dragMode = 0
+        mousePosition = (0, 0)
+
+
+if __name__ == '__main__':
+    video_file = sys.argv[1]
+
+    video_path = os.path.abspath(os.path.join(os.path.dirname(video_file), ".."))
+
+    if not os.path.exists('%s/crop_output' % video_path):
+        os.mkdir('%s/crop_output' % video_path)
+
+    read_video(video_file, '%s/crop_output' % video_path)
---
+++ scripts/preprocessing.py
@@ -0,0 +1,144 @@
+import cv2
+import numpy as np
+import sys
+import os
+import h5py
+import scipy.io
+import logging
+
+if __name__ == '__main__':
+
+    input_width = 151
+    input_height = 95
+    data_size = 422
+
+    data_root = sys.argv[1]
+    join = os.path.join
+    err_data = [14, 15, 16, 17, 18, 19, 45, 46, 201, 202, 203, 204, 231, 232, 233, 234, 235, 236, 237, 238, 239, 303, 304, 305, 306, 307, 308, 309, 335, 336, 337, 338, 342, 343, 344, 345, 346, 347, 366, 367, 368, 370, 371, 372, 373]
+
+    train_file = join(data_root, 'pixy_train.h5')
+    test_file = join(data_root, 'pixy_test.h5')
+
+    # label_mat = join(data_root, 'joints.mat')
+    # image_root = join(data_root, 'images')
+    #
+    # mat_data = scipy.io.loadmat(label_mat)['joints']
+    # data_size = mat_data.shape[2]
+
+    # logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    image_data = []
+    label_data = []
+
+    for i in range(data_size):
+
+        logger.info('Get %d Rows in dataset', i)
+
+        if i in err_data:
+            continue
+
+        image_path = join(data_root, '%d.jpg' % i)
+        label_path = join(data_root, '%d.mat' % i)
+
+        mat_data = scipy.io.loadmat(label_path)['output_%d' % i]
+        print mat_data.shape
+
+        image = cv2.imread(image_path)
+        [height, width] = image.shape[:2]
+
+        scale_w = input_width / float(width)
+        scale_h = input_height /float(height)
+
+        image = cv2.resize(image, (input_width, input_height))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # image = image / 255.0
+
+        fimage = image.copy()
+        fimage = cv2.flip(image, 1)
+
+        label_list = range(26)
+
+        for j in range(13):
+            cv2.circle(image, (int(mat_data[0, j * 2 + 1] * scale_w), int(mat_data[0, j * 2 + 2] * scale_h)), 2, (0,0,255), cv2.FILLED)
+            cv2.imshow('frame', fimage)
+            label_list[j * 2] = mat_data[0, j * 2 + 1] / float(width)
+            label_list[j * 2 + 1] = mat_data[0, j * 2 + 2] / float(height)
+
+        label_data.append(label_list)
+
+        label_list = range(26)
+        for j in range(13):
+            cv2.circle(fimage, (int((width - mat_data[0, j * 2 + 1]) * scale_w), int(mat_data[0, j * 2 + 2] * scale_h)), 2, (0,0,255), cv2.FILLED)
+            cv2.imshow('frame', fimage)
+            label_list[j * 2] = (width - mat_data[0, j * 2 + 1]) / float(width)
+            label_list[j * 2 + 1] = mat_data[0, j * 2 + 2] / float(height)
+        label_data.append(label_list)
+
+        image_data.append(image)
+        image_data.append(fimage)
+
+
+    label_data = np.array(label_data)
+    image_data = np.array(image_data)
+
+    index = range(len(label_data))
+    np.random.shuffle(index)
+
+    label_data = label_data[index]
+    image_data = image_data[index]
+
+    trainset = len(label_data) * 3 / 4
+
+    # train dataset
+    labels_train = label_data[:trainset]
+    images_train = image_data[:trainset]
+
+    # test dataset
+    labels_test = label_data[trainset:]
+    images_test = image_data[trainset:]
+
+    # write to hdf5
+    if os.path.exists(train_file):
+        os.remove(train_file)
+    if os.path.exists(test_file):
+        os.remove(test_file)
+
+    logger.info('Write train dataset to %s', train_file)
+    with h5py.File(train_file, 'w') as f:
+        f['label'] = labels_train.astype(np.float32)
+        f['data'] = images_train.astype(np.float32)
+
+    logger.info('Write test dataset to %s', test_file)
+    with h5py.File(test_file, 'w') as f:
+        f['label'] = labels_test.astype(np.float32)
+        f['data'] = images_test.astype(np.float32)
+
+    #
+    # for i in range(10):
+    #     image = image_data[i]
+    #     label = label_data[i]
+    #     [height, width] = image.shape[:2]
+    #     # image = image * 255.0
+    #     # image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    #
+    #     for j in range(13):
+    #         cv2.circle(image, (int(label[j * 2] * width), int(label[j * 2 + 1] * height)), 2, (0, 0, 255), cv2.FILLED)
+    #     cv2.imshow('frame', image)
+    #     cv2.waitKey(0)
+
+    # print np.array(label_data).shape
+    # image_data
+    # image = cv2.imread(image_file)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # image = image/255.0
+    # print image
+    # cv2.imshow('frame', image)
+    # cv2.waitKey(0)
+    # print image.shape
