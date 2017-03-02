8f25bb2b1730d30eddcd4a42de9b6655ab13352f
TingMiao <tingpan666@gmail.com>
2017-03-02 04:25:40 +0000

    CHG: solver

--- train/train.sh
+++ train/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
- $HOME/caffe/build/tools/caffe train \
+ caffe.bin train \
   --gpu 0 \
   --solver=solver.prototxt \
   --weights ./fine/model.caffemodel 2>&1 | tee -a ./log/my_model.log
