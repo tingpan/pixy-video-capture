beedc6ab172842776ddc8298cd30e136e07a4f73
TingMiao <tingpan666@gmail.com>
2017-04-20 02:25:28 +0800

    CHG: rebuild train folder

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,41 @@
-import h5py
+import modshogun as sg
 
-f = h5py.File('../train/data/mnist_test.h5', 'r' )
+# Create the CDenseFeatures and calculate distance using a certain distance metric
+train_set = sg.RealFeatures(dataset_object)
+distance = EuclideanDistance(train_set, train_set)
+
+
+# Train with k
+kmeans = KMeans(k, distance)
+kmeans.train()
+
+
+# Get radius and center
+c = kmeans.get_cluster_centers()
+r = kmeans.get_radiuses()
+
+
+
+import modshogun as sg
+
+# Create the CDenseFeatures
+train_set = sg.RealFeatures(dataset_object)
+
+
+# Create feature types and multiclass labels
+labels = sg.MulticlassLabels(position_data);
+
+
+# Create C4.5 tree object
+tree = sg.C45ClassifierTree()
+
+
+# Set training labels
+tree.set_labels(labels)
+tree.set_feature_types(position_data)
+
+
+# Train
+tree.train(train_set)
+
+
--- train/train.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
- caffe.bin train \
-  --gpu 0 \
-  --solver=solver.prototxt \
-  # --weights ./fine/model.caffemodel 2>&1 | tee -a ./log/my_model.log
-
