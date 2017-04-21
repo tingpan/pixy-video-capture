ff08b569b94bb177180412ea2f653d6dfac7bf49
TingMiao <tingpan666@gmail.com>
2017-04-21 23:59:34 +0800

    CHG: test

---
+++ scripts/visualize_curve.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from matplotlib import *
+from matplotlib.pyplot import *
+
+train_log = pd.read_csv("../train/heatmap-net/log/train_2017-04-20-15-11-22.log.train")
+test_log = pd.read_csv("../train/heatmap-net/log/train_2017-04-20-15-11-22.log.test")
+
+_, ax1 = subplots(figsize=(15, 10))
+ax2 = ax1.twinx()
+ax1.plot(train_log["NumIters"], train_log["loss"], alpha=0.4)
+# ax1.plot(test_log["NumIters"], test_log["loss"], 'g')
+
+ax1.set_xlabel('iteration')
+ax1.set_ylabel('train loss')
+# ax2.set_ylabel('test accuracy')
+
+savefig("./train_test_image.png") #save image as png
