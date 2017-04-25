16d1206e7284e60febcaf0980ffbb5a734185505
TingMiao <tingpan666@gmail.com>
2017-04-25 11:15:46 +0800

    CHG: deep pose

---
+++ train/deeppose/train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$2" = "" ]; then
+	echo "$0 caffe_path gpu_id [snap_iter]"
+	exit
+fi
+
+caffe_path=$1
+gpu_id=$2
+snap_iter=$3
+
+snap_dir="./snapshots/"
+snapfile="deeppose_train";
+pre_dir="./pre_trained"
+log_dir="./log"
+
+mkdir -p $log_dir
+mkdir -p $snap_dir
+
+if [ "$snap_iter" != "" ] &&  [ "$snap_iter" != "-1" ];
+    then snap_str="-snapshot $snap_dir/${snapfile}_iter_$snap_iter.solverstate";
+    else snap_str="-weights $pre_dir/simplenet.caffemodel";
+fi
+
+$caffe_path/build/tools/caffe train \
+-gpu $gpu_id -solver model/solver.prototxt 2>&1 | tee -a $log_dir/train_`date +%Y-%m-%d-%H-%M-%S`.log
